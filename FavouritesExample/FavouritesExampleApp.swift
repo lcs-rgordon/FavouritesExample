448bc18e1b4b74ce1951e8f5d242dfa4f945c5e1
Russell Gordon <github@russellgordon.ca>
2022-04-04 14:23:38 -0400

    Initial Commit

---
+++ FavouritesExample/FavouritesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesExampleApp.swift
+//  FavouritesExample
+//
+//  Created by Russell Gordon on 2022-04-04.
+//
+
+import SwiftUI
+
+@main
+struct FavouritesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
